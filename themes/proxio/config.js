fa1f7c2840d9b13cf56501da8ef75c3c87fe59df
Ieyache <1423222022@qq.com>
2025-05-15 16:29:36 +0800

    Update config.js

--- themes/proxio/config.js
+++ themes/proxio/config.js
@@ -197,10 +197,6 @@ const CONFIG = {
                     href: 'https://blog.tangly1024.com'
                 },
                   {
-                    title: '案例作品',
-                    href: 'https://www.tangly1024.com'
-                },
-                  {
                     title: '设计理念',
                     href: 'https://www.tangly1024.com'
                 }
